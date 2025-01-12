skyres17/android_packages_apps_Settings

Fixup UI for Settings categories (#155)
* Settings: Assign top-level tiles a category by default

Android 15 introduced new preference fragment category grouping.

Most external Device-specific "Parts" settings implementations don't pass
category information metadata so DashboardFragment can group them with
similar tiles.

Create a fall-through category and modify logic to group added settings
tiles together for graphical consistency.

Adds "crdroid_device_parts_settings" key to ACCOUNT_INJECTED_KEYS array.

To use, make sure to include the following entries in the main fragment
<activity> entry in your Extras/Parts/Settings AndroidManifest.xml:

    <meta-data
        android:name="com.android.settings.category"
        android:value="com.android.settings.category.ia.homepage" />
    <meta-data
        android:name="com.android.settings.keyhint"
        android:value="crdroid_device_parts_settings" />

Depends on commit:
https://github.com/crdroidandroid/android_packages_apps_Settings/commit/318019ce9ec2c24513c6f4b081a7d05c81befd2f

src/com/android/settings/dashboard/DashboardFragment.java
@@ -79,7 +79,8 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
 
     private static final List<String> ACCOUNT_INJECTED_KEYS = Arrays.asList(
         "top_level_google",
-        "dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity"
+        "dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity",
+        "crdroid_device_parts_settings"
     );
 
     private static final List<String> SECURITY_PRIVACY_INJECTED_KEYS = Arrays.asList(
@@ -573,10 +574,13 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
                         group = screen.findPreference("top_level_account_category");
                     } else if (SECURITY_PRIVACY_INJECTED_KEYS.contains(key)) {
                         group = screen.findPreference("top_level_security_privacy_category");
+                    } else {
+                        group = screen.findPreference("top_level_filthy_casuals");
                     }
                     if (group instanceof PreferenceCategory) {
                         ((PreferenceCategory) group).addPreference(pref);
                     } else {
+                        // Should never get here now
                         screen.addPreference(pref);
                     }
                 }

GioKas93/packages_apps_Settings

src/com/android/settings/dashboard/DashboardFragment.java
@@ -78,7 +78,8 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
     private static final long TIMEOUT_MILLIS = 50L;
 
     private static final List<String> ACCOUNT_INJECTED_KEYS = Arrays.asList(
-        "dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity"
+        "dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity",
+        "crdroid_device_parts_settings"
     );
 
     private static final List<String> SECURITY_PRIVACY_INJECTED_KEYS = Arrays.asList(
@@ -610,6 +611,8 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
                         group = screen.findPreference("top_level_account_category");
                     } else if (SECURITY_PRIVACY_INJECTED_KEYS.contains(key)) {
                         group = screen.findPreference("top_level_security_privacy_category");
+                    } else {
+                        group = screen.findPreference("top_level_category_undefined");
                     }
                     // Order the prefs within their respective category
                     if (KEY_ORDER.containsKey(key)) {
@@ -618,6 +621,7 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
                     if (group instanceof PreferenceCategory) {
                         ((PreferenceCategory) group).addPreference(pref);
                     } else {
+                        // Should never get here now
                         screen.addPreference(pref);
                     }
                 }